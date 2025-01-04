ba5ddfdb07ec7c8c28002e86bf47c35df27df8ac
AdarshBelnekar <118291123+AdarshBelnekar@users.noreply.github.com>
2025-01-04 18:33:55 +0530

    Update server.js
    asdd port again

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db');
 const bookingRoutes = require('./routes/bookingRoutes.js'); // Import booking routes
 
 const app = express();
-const PORT = "https://booking-table-43da-jim116fxw-adarshbelnekars-projects.vercel.app"
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
