76469a529f4aeaa0a6512a5dc1efad7dec417404
AdarshBelnekar <118291123+AdarshBelnekar@users.noreply.github.com>
2025-01-04 20:08:40 +0530

    Update TableComponent.js
    fetch file

--- table-booking-system/src/app/components/TableComponent.js
+++ table-booking-system/src/app/components/TableComponent.js
@@ -31,7 +31,7 @@ const TableComponent = () => {
     if (!confirmDelete) return;
 
     try {
-      const response = await fetch(`http://localhost:5000/api/book/${id}`, {
+      const response = await fetch(`https://bookingtable-g9lx.onrender.com`, {
         method: "DELETE",
       });
 
